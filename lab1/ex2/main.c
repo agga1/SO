dabcf5c6fa3065f4f13cd0aa46b7d27ed9997507
agga1 <agud100@gmail.com>
2020-03-11 16:55:20 +0100

    ex 2 raport with comments

--- lab1/ex2/main.c
+++ lab1/ex2/main.c
@@ -12,15 +12,14 @@
 #include <error.h>
 #include <stdbool.h>
 #include "library.h"
-#define N 7
+#define N 8
 
 void append_file(FILE* result_file, char* data){
-    fprintf(result_file, "%s\n", data);
+    fprintf(result_file, "%s", data);
 }
 void write_time(FILE *result_file, double r, double u, double s){
-    fprintf(result_file, "REAL_TIME: %fns\n", r);
-    fprintf(result_file, "USER_TIME: %fµs\n", u);
-    fprintf(result_file, "SYSTEM_TIME: %fµs\n", s);
+    fprintf(result_file, "\nReal time |  User time | System time: \n");
+    fprintf(result_file, "%f\t%f\t%f\n\n", r, u, s);
 }
 bool in(char* el, char ** str_array, int ar_len){
     for(int i = 0; i < ar_len; i++) {
@@ -33,10 +32,11 @@ double calculate_time(clock_t start, clock_t end) {
     return (double) (end - start) / sysconf(_SC_CLK_TCK);
 }
 int main(int argc, char **argv){
-    char *commands[N]= {"create_table", "compare_pairs", "remove_block", "remove_operation", "save_block", "start_time", "clear_file"};
+    char *commands[N]= {"create_table", "compare_pairs", "remove_block", "remove_operation", "save_block", "start_time",
+                        "clear_file", "comment"}; // file management - must be at the beginning!!!
 
     //// time measurement
-    bool count_time = false;
+//    bool count_time = false;
     struct tms **tms_time = malloc(2 * sizeof(struct tms *));
     clock_t real_time[2];
     for (int i = 0; i < 2; i++) tms_time[i] = (struct tms *) malloc(sizeof(struct tms *));
@@ -50,10 +50,10 @@ int main(int argc, char **argv){
     int nr=1;
     if(strcmp(argv[nr], commands[6])==0) {result_file = fopen(path, "w"); nr++;}
     else result_file = fopen(path, "a");
-    append_file(result_file, "\texecuted operations:");
+    if(strcmp(argv[nr], commands[7])==0)  {nr++; append_file(result_file, argv[nr]); nr++;}
 
     if(strcmp(argv[nr], commands[0])!=0) {
-        printf("command chain must start with %s!", commands[0]);
+        printf("command chain must start with %s! found%s", commands[0], argv[nr]);
         return 1;
     }
 
@@ -77,29 +77,29 @@ int main(int argc, char **argv){
             if(length<=5) snprintf(cmd, 256, "%s size small", commands[1]);
             else if(length<20) snprintf(cmd, 256, "%s size medium", commands[1]);
             else snprintf(cmd, 256, "%s size large", commands[1]);
-            if(count_time) append_file(result_file, cmd);
+//            if(count_time) append_file(result_file, cmd);
 
             i--;
         }
         else if(strcmp(argv[i], commands[2])==0){ // remove block
             remove_block(atoi(argv[++i]), ma);
-            if(count_time) append_file(result_file, commands[2]);
+//            if(count_time) append_file(result_file, commands[2]);
 
         }
         else if(strcmp(argv[i], commands[3])==0){ // remove operation
             int block_nr = atoi(argv[++i]);
             int ed_op_nr = atoi(argv[++i]);
             remove_ed_op(block_nr, ed_op_nr, ma);
-            if(count_time) append_file(result_file, commands[3]);
+//            if(count_time) append_file(result_file, commands[3]);
         }
         else if(strcmp(argv[i], commands[4])==0){ // save block
             int block_nr = atoi(argv[++i]);
             save_block(block_nr, ma);
-            if(count_time) append_file(result_file, commands[4]);
+//            if(count_time) append_file(result_file, commands[4]);
         }
         else if(strcmp(argv[i], commands[5])==0){ // start time
             real_time[0] = times(tms_time[0]);
-            count_time=true;
+//            count_time=true;
         }
     }
 
