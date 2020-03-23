1cea1102f776ed1b670acd43f4710811407f00d3
agga1 <agud100@gmail.com>
2020-03-23 18:33:39 +0100

    zad 1 done

---
+++ DutkaAgnieszka/cw03/zad1/main.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <linux/limits.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <values.h>
+#include <dirent.h>
+#define __USE_XOPEN_EXTENDED 1
+#include <ftw.h>
+#include <sys/wait.h>
+
+const char* commands[]= {"-mtime", "-atime", "-maxdepth"};
+static struct timespec initialization_time;
+char * type_to_string(int f_type);
+char * time_to_string(time_t time);
+char *dir();
+
+int count=0;
+struct filter{
+    bool on;
+    char modifier;
+    long value;
+};
+struct settings{
+    struct filter *mtime_fltr;
+    struct filter *atime_fltr;
+    long max_depth;
+};
+void set_filter(struct filter *fltr, char *val);
+struct filter mfilter = {false, ' ', 0};
+struct filter afilter = {false, ' ', 0};
+struct settings sett = {&mfilter, &afilter, LONG_MAX};
+bool filter_by_time(time_t time, struct filter *flter){
+    if(!flter->on) {return true;}
+    int diff = (int) ((initialization_time.tv_sec - time) / 86400);
+    switch (flter->modifier){
+        case'+':
+            return diff > flter->value;
+        case'-':
+            return diff < flter->value;
+        default:
+            return diff == flter->value;
+    }
+}
+int summarize(const char *fpath, const struct stat *stats, int typeflag, struct FTW *ftwbuf){
+    if (sett.max_depth < ftwbuf->level || fpath[sizeof(fpath)-1]=='.') return 0;
+    time_t mtime = stats->st_mtim.tv_sec;
+    time_t atime = stats->st_atim.tv_sec;
+    if( !filter_by_time(atime, sett.atime_fltr) ||
+        !filter_by_time(mtime, sett.mtime_fltr)){
+        return 0;
+    }
+    printf("%s | type: %s | total_links: %lu  | size: %ld bytes | atime: %s | mtime: %s\n",
+                fpath, type_to_string(typeflag), stats->st_nlink, stats->st_size,
+               time_to_string(atime), time_to_string(mtime));
+    int s;
+    if(typeflag == FTW_D) {
+        pid_t child_pid;
+        child_pid = vfork();
+        if(child_pid == 0 ) {
+            printf("\n\n%s\n", fpath);
+            printf("Child PID:%d\n",(int)getpid());
+            execlp("ls", "ls", "-l", fpath, NULL);
+
+        } else {
+            wait(&s);
+        }
+    }
+    count++;
+    return 0;
+}
+int main(int argc, char **argv) {
+    clock_gettime(CLOCK_REALTIME, &initialization_time);
+    char *path = dir();
+
+    for (int nr=1; nr < argc; nr++) {
+        if (strcmp(argv[nr], commands[0]) == 0) { // mtime
+            char *val = argv[++nr];
+            set_filter(sett.mtime_fltr, val);
+        }else
+            if (strcmp(argv[nr], commands[1]) == 0) { // atime
+            char *val = argv[++nr];
+            set_filter(sett.atime_fltr, val);
+        }else
+            if (strcmp(argv[nr], commands[2]) == 0) { // max depth
+            sett.max_depth = strtol(argv[++nr], NULL, 10);
+        }else {
+            strcpy(path, argv[nr]);
+        }
+    }
+    nftw(path, summarize, 0, FTW_PHYS);
+    return 0;
+}
+char *dir() {
+    char *cwd = calloc(PATH_MAX, sizeof(char));
+    getcwd(cwd, PATH_MAX);
+    return cwd;
+}
+void set_filter(struct filter *fltr, char* val) {
+    fltr->on = true;
+    fltr->modifier = val[0];
+    fltr->value = abs((int) strtol(val, NULL, 10));
+}
+char * type_to_string(int f_type){
+    switch (f_type) {
+        case FTW_D :
+            return  "dir";
+        case FTW_DNR:
+            return "unread dir";
+        case FTW_SL:
+            return  "slink";
+        case FTW_NS:
+            return  "unstable file";
+        case FTW_F:
+            return  "file";
+        default:
+            return "unknown";
+    }
+}
+char * time_to_string(time_t time){
+    size_t size = 256;
+    char *res = calloc(size, sizeof(char));
+    struct tm *local = localtime(&time);
+    strftime(res, size, "%Y/%m/%d, %H:%M:%S", local);
+    return res;
+}
+
