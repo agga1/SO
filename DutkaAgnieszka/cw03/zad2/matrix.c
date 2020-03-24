6719dececea5d36e7927a3dcd13ed75322b52957
agga1 <agud100@gmail.com>
2020-03-24 23:14:57 +0100

    helper

--- DutkaAgnieszka/cw03/zad2/matrix.c
+++ DutkaAgnieszka/cw03/zad2/matrix.c
@@ -12,29 +12,67 @@
 #include <sys/wait.h>
 #include <time.h>
 #include "matrix_manage.c"
-int pair_number = 0;
+int total_pairs = 0;
 const char* out_folder = "tmp";
 struct Task{
     int idx;
     int col;
 };
+int task_for_pair(int pair_idx, int total_tasks){
+    char buffer[PATH_MAX + 1];
+    sprintf(buffer, ".tmp/tasks%03d", pair_idx);
+    FILE* tasks_file = fopen(buffer, "r+");
+    int fd = fileno(tasks_file);
+    flock(fd, LOCK_EX);
 
+    char* tasks = calloc(total_tasks + 1, sizeof(char));
+    fseek(tasks_file, 0, SEEK_SET);
+    fread(tasks, 1, total_tasks, tasks_file);
+
+    char* task_ptr_offset = strchr(tasks, '0');
+    int task_index = task_ptr_offset != NULL ? task_ptr_offset - tasks : -1;
+
+    if (task_index >= 0) {
+        tasks[task_index] = '1';
+        fseek(tasks_file, 0, SEEK_SET);
+        fwrite(tasks, 1, total_tasks, tasks_file);
+        fflush(tasks_file);
+    }
+
+    free(tasks);
+
+    flock(fd, LOCK_UN);
+    fclose(tasks_file);
+
+    return task_index;
+}
 struct Task get_task() {
 
     struct Task task;
     task.col = -1;
     task.idx = -1;
-    for(int i=0; i< pair_number; i++){
+    for(int i=0; i< total_pairs; i++){
 
         char* task_filename = calloc(100, sizeof(char));
         sprintf(task_filename, "%s/tasks%d",out_folder, i);
         FILE* tasks_file = fopen(task_filename, "r+");
         int fd = fileno(tasks_file);
         flock(fd, LOCK_EX);
-
         char* tasks = calloc(1000, sizeof(char));
         fseek(tasks_file, 0, SEEK_SET);
         fread(tasks, 1, 1000, tasks_file);
+
+//        char* task_ptr_offset = strchr(tasks, '0');
+//        int task_index = task_ptr_offset != NULL ? task_ptr_offset - tasks : -1;
+//
+//        if (task_index >= 0) {
+//            tasks[task_index] = '1';
+//            fseek(tasks_file, 0, SEEK_SET);
+//            fwrite(tasks, 1, tasks_count, tasks_file);
+//            fflush(tasks_file);
+//        }
+//
+//        free(tasks);
 
         char* task_first_zero = strchr(tasks, '0');
         int task_index = task_first_zero != NULL ? task_first_zero - tasks : -1;
@@ -102,6 +140,19 @@ void calc_col_in_mx(char *a_file, char *b_file, int col, char *c_file) {
 int worker_function(char** a, char** b, int max_time, int mode, char **out_file) {
     time_t start_time = time(NULL);
     int task_nr = 0;
+    int pair_idx =0;
+//    while (pair_idx<total_pairs) {
+//        if ((time(NULL) - start_time) >= max_time) {
+//            printf("max_time reached\n");
+//            break;
+//        }
+//        int col = task_for_pair(pair_idx, b[pair_idx]->col);
+//        if(mode == MODE_JOINT)
+//            calc_col_in_mx(a[task.idx], b[task.idx], task.col, out_file[task.idx]);
+//        else
+//            calc_separate_col(a[task.idx], b[task.idx], task.col, task.idx);
+//        task_nr++;
+//    }
     while (1) {
         if ((time(NULL) - start_time) >= max_time) {
             printf("max_time reached\n");
@@ -172,7 +223,7 @@ int main(int argc, char* argv[]) {
 
         pairs_nr++;
     }
-    pair_number = pairs_nr;
+    total_pairs = pairs_nr;
 
     pid_t* processes = calloc(workers_nr, sizeof(int));
     for (int i = 0; i < workers_nr; i++) {
