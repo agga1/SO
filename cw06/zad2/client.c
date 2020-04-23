a21103169c7624fc4626fe2baafd51af89a2e41e
agness <agud100@gmail.com>
2020-04-23 19:59:51 +0200

    2 working with mq_notify!!!

--- cw06/zad2/client.c
+++ cw06/zad2/client.c
@@ -14,8 +14,8 @@
 char clientPath[50];
 int running = true;
 int clientID = -1;
-mqd_t serverQueId = -1;
-mqd_t clientQueId = -1;
+mqd_t serverQueue = -1;
+mqd_t clientQueue = -1;
 
 int peerQueue = -1;  // id of connected other client, if present
 int inputProcess = -1; // proccess handling inputs from command line
@@ -25,38 +25,55 @@ void awaitClientId();
 void send(int to, mtype type, char *text);
 void handleInput(char *input); // input process
 static void sigHandler(int sig); // input process
-void handleQueue(struct Message* msg); // main process (handle incoming messages)
-
+static void notificationHandler(union sigval sv);
+static void handleQueue(char *msg, unsigned int mtype); // main process (handle incoming messages)
+void set_notification();
 int main(){
     // turn off stdout buffering so messages are visible immediately
 //    if (setvbuf(stdout, NULL, _IONBF, 0) != 0) perrorAndQuit("cant change buffering mode");
     // get server queue
-    serverQueId = mq_open(SERVER_QUEUE, O_RDWR);
-    if (serverQueId == -1) perrorAndQuit("cant open serverQueue, make sure server running");
+    serverQueue = mq_open(SERVER_QUEUE, O_RDWR);
+    if (serverQueue == -1) perrorAndQuit("cant open serverQueue, make sure server running");
     // get client queue
     struct mq_attr attr;
     attr.mq_maxmsg = 4;
     attr.mq_msgsize = MSGSIZE;
     snprintf(clientPath, 50, "/%d", getpid());
-    clientQueId = mq_open(clientPath, O_CREAT | O_RDONLY | O_EXCL, 0666, &attr);
-    if (clientQueId == -1) perrorAndQuit("clientQueueId problem");
-    printf("Created client queue with id %d.\n", clientQueId);
-    catchSignal(SIGINT, sigHandler);  // handle info that peerId is waiting
-
-    send(serverQueId, INIT, clientPath);
+    clientQueue = mq_open(clientPath, O_CREAT | O_RDONLY | O_EXCL, 0666, &attr);
+    if (clientQueue == -1) perrorAndQuit("clientQueueId problem");
+    printf("Created client queue with id %d.\n", clientQueue);
+    catchSignal(SIGINT, sigHandler);  // Ctrl+C
+    catchSignal(SIGUSR1, notificationHandler);
+    send(serverQueue, INIT, clientPath);
     awaitClientId();
-    closeAndQuit(clientQueId, clientPath);
+    char input[MSG_LEN + 16]; // command + message text; read from terminal
+    set_notification();
+    while(running){
+        memset(input, '\0', MSG_LEN + 16);
+        fgets(input, MSG_LEN+16, stdin);
+
+        if (*input != '\0')
+            handleInput(input);
+    }
+    closeAndQuit(clientQueue, clientPath);
 
 }
+void set_notification(){
+    struct sigevent notification;
+    notification.sigev_notify = SIGEV_SIGNAL;
+    notification.sigev_signo = SIGUSR1;
+    notification.sigev_value.sival_ptr = &clientQueue;
+    if(mq_notify(clientQueue, &notification) == -1) perrorAndQuit("notification not set");
+}
 ssize_t receive_command(char *msg, int *type){
-    if (mq_receive(clientQueId, msg, MSGSIZE + 2, type) < 0){
+    if (mq_receive(clientQueue, msg, MSGSIZE + 2, type) < 0){
         printf("%s", msg);
         perror("server: receiving message failed");
     }
     return 0;
 }
 
-void send(int to, mtype type, char *text) {
+void send(mqd_t to, mtype type, char *text) {
     char message[MSGSIZE];
     snprintf(message, MSGSIZE, "%d %d %s", type, clientID, text);
     if (mq_send(to, message, MSGSIZE, type) == -1)
@@ -68,10 +85,11 @@ void awaitClientId(){
     memset(msg, '\0', MSGSIZE+2);
     int type;
     receive_command(msg, &type);
-    if(type == NEW_CLIENT){
 
+    if(type == NEW_CLIENT){
+        clientID = atoi(msg);
     }
-    printf("got %s", msg);
+    printf("got id %d", clientID);
 }
 // inputProcess------------------
 void handleInput(char *input){
@@ -88,18 +106,16 @@ void handleInput(char *input){
     }
     switch (cmd){
         case CONNECT:
-            send(serverQueId, CONNECT, msg);
+            send(serverQueue, CONNECT, msg);
             break;
         case DISCONNECT:
-            send(serverQueId, DISCONNECT, msg);
+            send(serverQueue, DISCONNECT, msg);
             break;
         case STOP:
-            send(serverQueId, STOP, msg);
-            kill(getppid(), SIGKILL);
-            closeAndQuit(clientQueId, clientPath);
+            send(serverQueue, STOP, msg);
             break;
         case LIST:
-            send(serverQueId, LIST, msg);
+            send(serverQueue, LIST, msg);
             break;
         default:
             puts("unknown command");
@@ -108,39 +124,47 @@ void handleInput(char *input){
 
 static void sigHandler (int signum){
     if(signum==SIGINT){
-        send(serverQueId, STOP, "ok");
-//        kill(getppid(), SIGINT);
-        closeAndQuit(clientQueId, clientPath);
-    }
-    else if(signum == SIGUSR1){ // info that peerId is waiting
-        read(fd[0], &peerQueue, sizeof(peerQueue));
-        printf("peer Queue: %d\n", peerQueue);
+        send(serverQueue, STOP, "ok");
+//        usleep(5000);
+//        closeAndQuit(clientQueue, clientPath);
     }
 }
-// -------main process--------------------------
-void handleQueue(struct Message* msg){
+static void notificationHandler(union sigval sv) {
+    puts("here");
+    char msg[MSGSIZE+2];
+    memset(msg, '\0', MSGSIZE+2);
+    int type;
+    receive_command(msg, &type);
+    handleQueue(msg, type);
+    set_notification();
+}
+
+static void handleQueue(char *msg, unsigned int mtype){
+//    printf("handlig msg %s", msg);
     if (msg == NULL) return;
-    switch(msg->mtype){
+    switch(mtype){
         case STOP:
-            kill(inputProcess, SIGINT); // shut down from input process
+            // ready to be killed
+            usleep(10000);
+            if(peerQueue != -1) mq_close(peerQueue);  // cleanup peer queue
+            closeAndQuit(clientQueue, clientPath);
             break;
         case DISCONNECT:
+            mq_close(peerQueue);
             peerQueue = -1;
-            write(fd[1], &peerQueue, sizeof(peerQueue));
-            kill(inputProcess, SIGUSR1);
             break;
         case CONNECT:
-            peerQueue = atoi(msg->msg);
-            write(fd[1], &peerQueue, sizeof(peerQueue));
-            kill(inputProcess, SIGUSR1);
+            peerQueue = mq_open(msg, O_WRONLY);
             if(peerQueue != -1)
                 puts("you are in chat mode now. type DISCONNECT to disconnect");
+            else perror("connecting fail");
+
             break;
         case MESSAGE:
-            printf("[friend]:%s", msg->msg);
+            printf("[friend]:%s", msg);
             break;
         case LIST:
-            printf("%s", msg->msg);
+            printf("%s", msg);
             break;
         default:
             puts("unhandled signal");
--- cw06/zad2/server.c
+++ cw06/zad2/server.c
@@ -10,25 +10,27 @@
 #include <sys/msg.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include "config.h"
 bool running = true;
 mqd_t serverQueID =-1;
 
-typedef struct Client{
+struct Client{
     int queueId;
+    char path[16];
     int peerId;
-} client;
-client clients[MAX_CLIENTS + 1]; // client id begins from 1
+};
+struct Client clients[MAX_CLIENTS + 1] = { 0 }; // client id begins from 1
 int nextIdx = 1;
 int connectedClients = 0;
 
 void handleQueue(char *msg);
 void handleNewClient(char *clientPath);
-void handleStop(Message *msg);
-void handleList(Message *message);
-void handleConnect(Message *msg);
-void handleDisconnect(Message *msg);
+void handleStop(int clientId);
+void handleList(int clientId);
+void handleConnect(int id1, int id2);
+void handleDisconnect(int clientId);
 static void sigintHandler(int sig);
 
 int main()
@@ -42,8 +44,6 @@ int main()
     printf("Created server queue with id %d.\n", serverQueID);
 
     catchSignal(SIGINT, sigintHandler);  // handle CTRL + C
-
-    memset(clients, 0, (MAX_CLIENTS+1) * sizeof(client));
 
     char request[MSGSIZE+2];
     while(running){
@@ -66,19 +66,19 @@ void handleQueue(char *msg){
     int mtype = atoi(strtok(msg, " "));
     int clientId = atoi(strtok(NULL, " "));
     char *args = strtok(NULL, "\0");
-    printf("mtype: %d client %d args %s", mtype, clientId, args);
+    printf("mtype: %d client %d args %s\n", mtype, clientId, args);
     switch(mtype){
         case STOP:
-//            handleStop(msg);
+            handleStop(clientId);
             break;
         case DISCONNECT:
-//            handleDisconnect(msg);
+            handleDisconnect(clientId);
             break;
         case LIST:
-//            handleList(msg);
+            handleList(clientId);
             break;
         case CONNECT:
-//            handleConnect(msg);
+            handleConnect(clientId, atoi(args));
             break;
         case INIT:
             handleNewClient(args);
@@ -89,35 +89,36 @@ void handleQueue(char *msg){
 }
 void send(mtype type, int clientID, char *msg ) {
     char message[MSGSIZE];
-    snprintf(message, MSGSIZE, "%d %d %s", type, clientID, msg);
-    printf("message %s", message);
+    snprintf(message, MSGSIZE, "%s", msg);
+    printf("\nsent %s", message);
     if (mq_send(clients[clientID].queueId, message, MSGSIZE, type) == -1)
         perror("message not sent");
 }
-void handleStop(Message *msg){
-//    // TODO del queue
-//    clients[msg->clientId].queueId = 0;
-//    clients[msg->clientId].peerId = 0;
-//    connectedClients -=1;
-//    mq_close(queueID);
-//
-//    puts("client removed");
+void handleStop(int clientId){
+    send(STOP, clientId, "queue closing on server");
+    clients[clientId].queueId = 0;
+    clients[clientId].peerId = 0;
+    usleep(1000);
+    mq_close(clients[clientId].queueId);
+    connectedClients -=1;
+    puts("client removed");
 }
 void handleNewClient(char *clientPath){
     if(nextIdx > MAX_CLIENTS){
         puts("clients' list full");
         return;
     }
-
     clients[nextIdx].queueId = mq_open(clientPath, O_WRONLY);
+    snprintf(clients[nextIdx].path, 16, "%s", clientPath);
     clients[nextIdx].peerId = 0;
     connectedClients += 1;
     nextIdx++;
-
-    send(NEW_CLIENT, nextIdx-1, "ok");
+    char id[10];
+    sprintf(id, "%d", nextIdx -1);
+    send(NEW_CLIENT, nextIdx-1, id);
 }
 
-void handleList(Message *message) {
+void handleList(int clientId) {
     char text[MSG_LEN];
     strcpy(text, "active clients:\n");
     for(int i=0;i<MAX_CLIENTS+1;i++){
@@ -125,55 +126,53 @@ void handleList(Message *message) {
             char line[32];
             snprintf(line, 32, "clientId %d, available: %d", i, clients[i].peerId == 0);
             strcat(text, line);
-            if(i == message->clientId) strcat(text, " [self]");
+            if(i == clientId) strcat(text, " [self]");
             strcat(text, "\n");
         }
     }
-//    send(LIST, message->clientId, text);
+    send(LIST, clientId, text);
 }
 bool available(int id){
     if(id<0 || id> MAX_CLIENTS || clients[id].queueId ==0 || clients[id].peerId != 0)
         return false;
     return true;
 }
-void handleConnect(Message *msg) {
-//    int id1 = msg->clientId;
-//    int id2 = atoi(msg->msg);
-//    if(!available(id1)) {printf("client nr %d is unavailable", id1);
-//        return;}
-//    if(!available(id2)) {printf("client nr %d is unavailable\n", id2);
-//        return;}
-//
-//    printf("connecting [%d] and [%d]\n", id1, id2);
-//    char msg1[MSG_LEN];
-//    char msg2[MSG_LEN];
-//    sprintf(msg1, "%d", clients[id2].queueId);
-//    sprintf(msg2, "%d", clients[id1].queueId);
-//
-//    send(CONNECT, id1, msg1);
-//    send(CONNECT, id2, msg2);
-//
-//    clients[id1].peerId = id2;
-//    clients[id2].peerId = id1;
+void handleConnect(int id1, int id2) {
+    printf("connecting [%d] and [%d] ", id1, id2);
+    if(!available(id1)) {printf("client nr %d is unavailable", id1);
+        return;}
+    if(!available(id2)) {printf("client nr %d is unavailable\n", id2);
+        return;}
+
+    char msg1[MSG_LEN];
+    char msg2[MSG_LEN];
+    sprintf(msg1, "%s", clients[id2].path);
+    sprintf(msg2, "%s", clients[id1].path);
+
+    send(CONNECT, id1, msg1);
+    send(CONNECT, id2, msg2);
+
+    clients[id1].peerId = id2;
+    clients[id2].peerId = id1;
 }
-void handleDisconnect(Message *msg) {
-//    int id1 = msg->clientId;
-//    int id2 = clients[id1].peerId;
-//    printf("disconnecting [%d] and [%d]\n", id1, id2);
-//    send(DISCONNECT, id1, "");
-//    send(DISCONNECT, id2, "");
-//
-//    clients[id1].peerId = 0;
-//    clients[id2].peerId = 0;
+void handleDisconnect(int clientId) {
+    int id1 = clientId;
+    int id2 = clients[id1].peerId;
+    printf("disconnecting [%d] and [%d]\n", id1, id2);
+    send(DISCONNECT, id1, "");
+    send(DISCONNECT, id2, "");
+
+    clients[id1].peerId = 0;
+    clients[id2].peerId = 0;
 }
 static void sigintHandler (int signum){
     for (int i = 0; i < MAX_CLIENTS + 1; ++i) {
         if(clients[i].queueId > 0){
-//            send(STOP, i, "from server");
+            handleStop(i);
         }
     }
     if(connectedClients != 0)
-        printf("closing with connected clients: %d left", connectedClients);
+        printf("\nclosing with connected clients: %d left", connectedClients);
 
     closeAndQuit(serverQueID, SERVER_QUEUE);
 }
