24be07a52eb29b75146fe06e48a59dd181b29ec9
agga1 <agud100@gmail.com>
2020-05-14 21:29:16 +0200

    working fkin perfectly

--- cw09/zad1/main.c
+++ cw09/zad1/main.c
@@ -14,6 +14,7 @@ pthread_t barberThId;
 pthread_t *clientsThIds;
 int waitingClients=0;
 int queueStart=0;
+int atBarberSeat=-1;
 pthread_mutex_t waitingRoomMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t newArrival = PTHREAD_COND_INITIALIZER;
 
@@ -27,46 +28,60 @@ void barber(){
 
     while(1){
         pthread_mutex_lock(&waitingRoomMutex);
-        if(waitingClients == 0){
+        if(waitingClients == 0 && atBarberSeat==-1){ // nothing to do
             puts("Barber: i'm gonna sleep now.");
             pthread_cond_wait(&newArrival, &waitingRoomMutex);
         }
-        int client = waitingRoom[queueStart];
-        printf("----got client %d from seat %d\n", client, queueStart);
-        // free seat and move queue
-        waitingRoom[queueStart] = -1;
-        waitingClients--;
-        queueStart = (queueStart + 1) % K;
+        if(atBarberSeat == -1){
+            // get client, free seat and move queue
+            atBarberSeat = waitingRoom[queueStart];
+            waitingRoom[queueStart] = -1;
+            waitingClients--;
+            queueStart = (queueStart + 1) % K;
+        }
+        printf("%d more clients waiting, shaving client [%d]\n", waitingClients, atBarberSeat);
+        pthread_mutex_unlock(&waitingRoomMutex);
 
-        printf("%d more clients waiting, shaving client %d\n", waitingClients, client);
-        printf("queueStart %d\n", queueStart);
-        sleep(getRnd(2, 4));
-        pthread_cancel(clientsThIds[client]);
-        printf("shaved %d\n", client);
+        sleep(getRnd(3, 5));
+
+        pthread_mutex_lock(&waitingRoomMutex);
+        pthread_cancel(clientsThIds[atBarberSeat]);
+        printf("client %d shaved\n", atBarberSeat);
+        atBarberSeat = -1;
         pthread_mutex_unlock(&waitingRoomMutex);
     }
 }
+int findPlaceToSit(){
+    pthread_mutex_lock(&waitingRoomMutex);
+    int spot = (queueStart+waitingClients)%K; // next supposedly free spot
+    if(waitingRoom[spot] != -1)            // queue tail meets head
+        spot = -1; // no place in queue
+    pthread_mutex_unlock(&waitingRoomMutex);
+    return spot;
+}
 void client(int *idx){
-    printf("client nr %d, id %lu\n", *idx, clientsThIds[*idx]);
+
+    int spot = findPlaceToSit();
+    while(spot == -1){  // no place in the waiting room
+        printf("all seats taken; [%d]\n", *idx);
+        sleep(getRnd(4, 6));
+        spot = findPlaceToSit();
+    }
 
     pthread_mutex_lock(&waitingRoomMutex);
-
-    int spot = (queueStart+waitingClients)%K;
-    if(waitingRoom[spot] != -1) {puts("all taken"); printf("on spot %d", waitingRoom[spot]); return;}
-    waitingRoom[spot] = *idx; // take a seat
-    waitingClients ++;
-    printf("[%d] taking seat %d now waiting clients %d\n", *idx, spot, waitingClients);
-
-    if(waitingClients == 1){ // that means no one waits and shop is empty
+    if(waitingClients == 0 && atBarberSeat == -1){ // barber is free
+        atBarberSeat = *idx;  // sit at barber's
+        printf("I am waking up barber; [%d]\n", *idx);
         pthread_cond_broadcast(&newArrival);
-        printf("I am waking up barber, ID: %d\n\n", *idx);
-    }else{          // sit in the waiting room
-        printf("Waiting room, free seats: %d; ID: %d\n\n", K-waitingClients, *idx);
+    }
+    else{  // sit in the waiting room
+        waitingRoom[spot] = *idx;
+        waitingClients ++;
+        printf("Waiting room, free seats: %d; [%d]\n", K-waitingClients, *idx);
     }
     pthread_mutex_unlock(&waitingRoomMutex);
     pause();
 }
-
 
 int main(int argc, char **argv ) {
     srand(time(NULL));
@@ -74,7 +89,6 @@ int main(int argc, char **argv ) {
         printf("Error: buffering mode could not be changed!\n");
         exit(1);
     }
-
     if(argc != 3){
         printf("usage: K N (K - nr of seats in the waiting room, N - number of clients)");
         exit(1);
@@ -83,8 +97,8 @@ int main(int argc, char **argv ) {
     N = (int) strtol(argv[2], NULL, 10);
 
     waitingRoom = malloc((size_t) K*sizeof(int));
-    for (int i=0; i<K; ++i)    // Set the first 6 elements in the array
-        waitingRoom[i] = -1;
+    for (int i=0; i<K; ++i) waitingRoom[i] = -1;
+
     clientsThIds = calloc((size_t) N, sizeof(pthread_t));
 
     // making threads
@@ -92,12 +106,11 @@ int main(int argc, char **argv ) {
         perror("barber not created");
         exit(2);
     };
-    printf("barb id %lu\n", barberThId);
 
     int *args = calloc((size_t) N, sizeof(int));
     for (int i = 0; i < N; i++) {
         args[i] = i;
-        sleep(getRnd(1, 3));
+        sleep(getRnd(1, 2));
         if (pthread_create(&clientsThIds[i], NULL, (void *(*)(void *)) client, args+i))
             perror("client's thread creation problem");
     }
